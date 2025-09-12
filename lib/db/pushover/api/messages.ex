ed140865d9e20fdf298c37549a41fbe077390310
Lukas Wigger <lukas@wigger.dev>
2025-09-12 23:14:09 +0200

    fix: format

--- lib/db/pushover/api/messages.ex
+++ lib/db/pushover/api/messages.ex
@@ -20,7 +20,11 @@ defmodule Db.Pushover.Messages do
           {:ok, Req.Response.t()} | {:error, Exception.t()}
   def send(message) do
     Req.post!("https://api.pushover.net/1/messages.json",
-      json: %{message | user: Application.get_env(:db, :user), token: Application.get_env(:db, :token)}
+      json: %{
+        message
+        | user: Application.get_env(:db, :user),
+          token: Application.get_env(:db, :token)
+      }
     )
   end
 end
